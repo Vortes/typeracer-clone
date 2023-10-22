847d1360a66c79ca92b505f6cefe6706e0db131f
Alan <zeyu.weng@gmail.com>
2023-10-22 02:14:55 -0400

    finished error checking logic

--- typeracer-clone/src/App.js
+++ typeracer-clone/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const fetchData = async () => {
+    const response = await fetch("/api/")
+    const result = await response.json()
+    console.log(result)
+  }
+
+  const paragraph = "hello world"
+  let paragraphArray = paragraph.split("")
+  const [inputText, setInputText] = useState("")
+  const [textArray, setTextArray] = useState("")
+  const [errorIndexes, setErrorIndexes] = useState([])
+  let currentIndex = inputText.split("").length -1
+
+  const readInput = (e) => {
+    setInputText(e.target.value)
+    setTextArray(e.target.value.split(''))
+  }
+
+  const handleError = () => {
+    if(!errorIndexes.includes(currentIndex) && currentIndex < paragraphArray.length) {
+      setErrorIndexes([...errorIndexes, currentIndex])
+    }
+  }
+
+  const correctError = () => {
+    console.log("in here")
+    setErrorIndexes(errorIndexes.filter(error => error !== currentIndex))
+  }
+
+  useEffect(()=> {
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" onChange={readInput} value={inputText} />
+      <p>{paragraph}</p>
+      <p>{inputText}</p>
+
+      <p>paragraph: {paragraphArray.length}</p>
+
+      <p>current index: {currentIndex}</p>
+      {textArray[currentIndex] !== paragraphArray[currentIndex] ? handleError() : ""}
+      {textArray[currentIndex] === paragraphArray[currentIndex] && errorIndexes.includes(currentIndex) ? correctError() : ""}
+      <p>error at {errorIndexes}</p>
     </div>
   );
 }
