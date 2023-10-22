cc0c733f150b8d8231d555a35f02a742c53633f1
Alan <zeyu.weng@gmail.com>
2023-10-22 18:34:29 -0400

    initial steps finished

--- typeracer-clone/src/TypingWorkspace.js
+++ typeracer-clone/src/TypingWorkspace.js
@@ -12,22 +12,43 @@ const TypingWorkspace = ({
     currentIndex,
 }) => {
     const [paragraph, setParagraph] = useState("")
+    const [charClassNames, setCharClassNames] = useState([]);
 
     useEffect(()=> {
         const generatedParagraph = getRandomParagraph(35).join(" ")
         setParagraph(generatedParagraph)
+        setCharClassNames(new Array(generatedParagraph.length).fill("text-3xl text-textParagraph"));
     }, [])
 
     let paragraphArray = paragraph.split("");
 
+    const convertParagraphToHTMLArray = () => {
+    return paragraph.split("").map((char, index) => (
+        <span key={index} className={charClassNames[index]}>
+            {char}
+        </span>
+        ))
+    }   
+
+    const paragraphWithHTML = convertParagraphToHTMLArray()
+
     const readInput = (e) => {
-        setInputText(e.target.value);
+        setInputText(e.target.value);   
+
+        if (!timerOn) {
+            setTimerOn(true);
+        }
+
         if (timer === 0) {
             e.target.disabled = true
         }
 
-        if (!timerOn) {
-            setTimerOn(true);
+        if (e.target.value[currentIndex] === paragraphArray[currentIndex]) {
+            setCharClassNames(prevClassNames => {
+                const newClassNames = [...prevClassNames];
+                newClassNames[currentIndex] = "text-3xl text-textInput";
+                return newClassNames;
+            });
         }
 
         if (e.target.value[currentIndex] !== paragraphArray[currentIndex]) {
@@ -53,7 +74,10 @@ const TypingWorkspace = ({
     return (
         <div className="flex flex-col">
             <input className="border py-2" type="text" onChange={readInput} value={inputText} />
-            <p className="text-3xl text-textParagraph">{paragraph}</p>
+            {/* <p className="text-3xl text-textParagraph">{paragraph}</p> */}
+            <div>
+                {paragraphWithHTML}
+            </div>
             <p className="text-2xl text-textInput">{inputText}</p>
             <p className="text-lg text-error">error at {errorIndexes}</p>
         </div>
