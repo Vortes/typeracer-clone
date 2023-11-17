90b3e3f6450f38f9cd1fb03991662187a3ebedea
Alan <zeyu.weng@gmail.com>
2023-11-17 00:54:20 -0500

    testing out decoupling

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import { io } from 'socket.io-client';
 
-export const socket = io("https://typeracer-clone.pages.dev/");
+export const socket = io("https://typeracer-clone-backend-iwcj.onrender.com");
